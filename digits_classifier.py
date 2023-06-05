a6167ef8faf85f77fb9bbc016625f77f3f1880c3
yair8 <yair.shemer1@mail.huji.ac.il>
2023-06-05 23:44:16 +0300

    first commit

---
+++ digits_classifier.py
@@ -0,0 +1,69 @@
+from abc import abstractmethod
+import torch
+import typing as tp
+from dataclasses import dataclass
+
+
+
+@dataclass
+class ClassifierArgs:
+    """
+    This dataclass defines a training configuration.
+    feel free to add/change it as you see fit, do NOT remove the following fields as we will use
+    them in test time.
+    If you add additional values to your training configuration please add them in here with 
+    default values (so run won't break when we test this).
+    """
+    # we will use this to give an absolute path to the data, make sure you read the data using this argument. 
+    # you may assume the train data is the same
+    path_to_training_data_dir: str = "./train_files" 
+    
+    # you may add other args here
+
+
+class DigitClassifier():
+    """
+    You should Implement your classifier object here
+    """
+    def __init__(self, args: ClassifierArgs):
+        self.path_to_training_data = args.path_to_training_data_dir
+
+    @abstractmethod
+    def classify_using_eucledian_distance(self, audio_files: tp.Union[tp.List[str], torch.Tensor]) -> tp.List[int]:
+        """
+        function to classify a given audio using auclidean distance
+        audio_files: list of audio file paths or a a batch of audio files of shape [Batch, Channels, Time]
+        return: list of predicted label for each batch entry
+        """
+        raise NotImplementedError("function is not implemented")
+    
+    @abstractmethod
+    def classify_using_DTW_distance(self, audio_files: tp.Union[tp.List[str], torch.Tensor]) -> tp.List[int]:
+        """
+        function to classify a given audio using DTW distance
+        audio_files: list of audio file paths or a a batch of audio files of shape [Batch, Channels, Time]
+        return: list of predicted label for each batch entry
+        """
+        raise NotImplementedError("function is not implemented")
+
+
+    @abstractmethod
+    def classify(self, audio_files: tp.List[str]) -> tp.List[str]:
+        """
+        function to classify a given audio using auclidean distance
+        audio_files: list of ABSOLUTE audio file paths
+        return: a list of strings of the following format: '{filename} - {predict using euclidean distance} - {predict using DTW distance}'
+        Note: filename should not include parent path, but only the file name itself.
+        """
+        raise NotImplementedError("function is not implemented")
+    
+
+class ClassifierHandler:
+
+    @staticmethod
+    def get_pretrained_model() -> DigitClassifier:
+        """
+        This function should load a pretrained / tuned 'DigitClassifier' object.
+        We will use this object to evaluate your classifications
+        """
+        raise NotImplementedError("function is not implemented")
